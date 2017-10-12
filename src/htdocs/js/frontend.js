fca6ee7717f4ceda36c6d82fc843338cc46305a9
jcaple <capable2@gmail.com>
2017-10-12 03:55:50 -0400

    Search functionality working; Removed giphy API key

--- src/htdocs/js/frontend.js
+++ src/htdocs/js/frontend.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const CONFIG = {
-	GIPHY_API_KEY: '3sLRM5UvMfHIFIqsAur3cUq2nBppYUEd',
-	Giphy_SEARCH_URL: 'https://api.giphy.com/v1/gifs/search'
+	GIPHY_API_KEY: '',  // PROVIDE YOUR OWN GIPHY API KEY HERE!!!!!!
+	Giphy_SEARCH_URL: 'https://api.giphy.com/v1/gifs/search',
+	COOKIE_NAME: 'jcaple007-giphy_search'
 };
 
 let resultVue = new Vue({
@@ -19,29 +20,42 @@ let searchVue = new Vue({
 		searchPhrase: ''
 	},
 	methods: {
+		onSubmit: function() {},
 		btnSearchClick: function () {
 			console.log('Button CLicked');
-		  if (this.searchPhrase !== '' || this.searchPhrase !== undefined) {
-		  	console.log('Searching For: ' + this.searchPhrase);
-		  	resultVue.results = 'Searching...';
-		  	search(this.searchPhrase).then(function(results) {
-		  		resultVue.results = '';
-		  		resultVue.items = [];
+			if (this.searchPhrase !== '' || this.searchPhrase !== undefined) {
+				console.log('Searching For: ' + this.searchPhrase);
+			  	resultVue.results = 'Searching...';
+			  	search(this.searchPhrase).then(function(results) {
+			  		resultVue.results = '';
+			  		resultVue.items = [];
 
-				results.data.forEach(function(image) {
-					let imgURL = image.images.downsized.url;
-					resultVue.items.push({'img':imgURL, 'alt': image.slug});
-				});
-		  	});
-		  }
+					results.data.forEach(function(image) {
+						let imgURL = image.images.downsized.url;						
+						let shareURL = "https://www.facebook.com/plugins/share_button.php?href=" + image.embed_url + "&layout=button&size=small&mobile_iframe=true&appId=360349651081519&width=59&height=20";
+						resultVue.items.push({
+							'img':imgURL, 
+							'alt': image.slug, 
+							'embed': image.embed_url, 
+							'share': shareURL
+						});
+					});
+			  	});
+			}
 		}
 	}
 });
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
 function search(giphy_search) {
 	let url = CONFIG.Giphy_SEARCH_URL + "?api_key="
 		+ CONFIG.GIPHY_API_KEY + "&q=" 
-		+ giphy_search + "&limit=20&rating=r";
+		+ giphy_search + "&limit=30&rating=r&offset=" + getRandomInt(0,200);
 
 	let promise = Promise.resolve();
 	return promise.then(function() {
@@ -50,3 +64,8 @@ function search(giphy_search) {
 		});
 	});
 }
+
+function logout() {
+    document.cookie = CONFIG.COOKIE_NAME + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    window.location = '/login.html';
+};
--- src/lib/config.js
+++ src/lib/config.js
@@ -7,14 +7,7 @@ const CONFIG = {
 	env: 'DEV',
 	version: '0.0.1',
 	secret: 'l0phtingAndShippingCode',
-	'apiroot': '/api',
-
-	// Giphy API Configuration 
-	getGiphyAPIKey: function() {
-		// Set the API key as an environment variable
-		return process.env.GIPHY_API_KEY;
-	},
-	Giphy_URL: 'https://api.giphy.com/v1/gifs/search'
+	'apiroot': '/api'
 };
 
 module.exports = CONFIG;
